89fba699c83c8ac41efc20ee622492ab7576bfc8
AboveAverageGames <tjpayne97@gmail.com>
2024-02-09 12:10:40 +0000

    Coin Mechanics Furthered
    Added a auto counter to detect the amount of coins in the level + a debug.log saying when they are all collected.
    I made the coins be worth 10 points each and update the current score accordingly.

--- Assets/Scripts/Coins.cs
+++ Assets/Scripts/Coins.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 
 public class Coins : MonoBehaviour
 {
+    public int pointsItemIsWorth = 10;
+
+    public GameManagerScript gameManagerScript;
 
     private void OnCollisionEnter(Collision collision)
     {
+        //If Coin collides with player then it is destroyed + Updates the Score in the GameManagerScript
         if (collision.gameObject.CompareTag("Player"))
             {
+            gameManagerScript.totalCoinsCollected++;
             Destroy(gameObject);
+            gameManagerScript.UpdateScore(pointsItemIsWorth);
+            
         }
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Assigns the GameManagerScript to this object so I can access it when needed
+        gameManagerScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManagerScript>();
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/GameManagerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+    private int numberOfCoins;
+    public int totalCoinsCollected;
+    public int currentScore;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Finds the total number of coins in the scene at the start of the level
+        numberOfCoins = GameObject.FindGameObjectsWithTag("Coin").Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If statement to check if ALL THE COINS in the level have been collected
+        if (totalCoinsCollected == numberOfCoins)
+        {
+            Debug.Log("You got all the coins");
+        }
+        
+    }
+
+    //Updates the score, using the amount of points a collected item is worth
+    public void UpdateScore(int points)
+    {
+        //Updates the current score with the INT Points that is passed into the function
+        currentScore = currentScore + (points);
+        Debug.Log("The current score is" +  currentScore + " The total coins collected is " + totalCoinsCollected);
+    }
+}
